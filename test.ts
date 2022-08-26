8501532f163ed5d1a749c283eb39cab4bcadb9f0
hamza <hamzah@paritie.com>
2022-08-26 17:52:44 +0100

    added more test quetions

---
+++ test.ts
@@ -0,0 +1,7 @@
+
+let n:null = null;
+let u:undefined = undefined;
+//Nothing else can be assigned to them except theirselves.
+
+// n = undefined  TypeError. undefined not assignable to null
+// u = null  TypeError. null is not assignable to undefined
